a87de32b0463d63059ec53c79f1135c2540bc2f0
Nectarios UofA-TUC-FSF-PT-02-2020-U-C <npgockel@gmail.com>
2020-03-20 23:18:55 -0700

    Checkpoint Commit

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+
+
+
+$("#search").on("click", function() {
+    var city = $("#city").val();
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d6746b3e28e79b150c827fa250bf066e";
+
+    $.ajax({
+      method: "GET",
+      url: url
+    }).then(function(response) {
+      console.log(response);
+      $("#search-history").prepend("<p>response.name</p>")
+    });
+  });
+
