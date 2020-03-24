84078cd8bbdfe3f40af710fe476a8e1a169a042a
Nectarios UofA-TUC-FSF-PT-02-2020-U-C <npgockel@gmail.com>
2020-03-24 13:32:31 -0700

    post-tutor session

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,55 @@
-
-
-
 $("#search").on("click", function() {
-    var city = $("#city").val();
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d6746b3e28e79b150c827fa250bf066e";
+  var city = $("#city").val();
+  mainWeather(city);
+});
 
-    $.ajax({
-      method: "GET",
-      url: url
-    }).then(function(response) {
-      console.log(response);
-      $("#search-history").prepend(response.name)
-    });
+function mainWeather(search) {
+  var url = "http://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=d6746b3e28e79b150c827fa250bf066e&units=imperial";
+  console.log("You got clicked", search);
+  $.ajax({
+    method: "GET",
+    url: url
+  }).then(function(response) {
+    console.log(response);
+    console.log(response.main.temp, response.main.humidity, response.wind.speed);
+    var temp = $("<h1>").text("Temperature: " + response.main.temp);
+    var humidity = $("<h1>").text("Humidity: " + response.main.humidity);
+    var windspeed = $("<h1>").text("Windspeed: " + response.wind.speed);
+    var cityh1 = $("<h1>").text(search);
+    $("#info-city").prepend(cityh1, temp, humidity, windspeed);
+    fiveday(search);
   });
+}
 
+function fiveday(search) {
+  var url = "http://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=d6746b3e28e79b150c827fa250bf066e&units=imperial";
+  console.log("You got clicked", search);
+  $.ajax({
+    method: "GET",
+    url: url
+  }).then(function(response) {
+    console.log(response);
+    var fiveDayArray = [];
+    for (var i = 0; i < response.list.length; i++) {
+      //console.log('single dude from array',response.list[i]);
+      //console.log('single dudes time', response.list[i].dt_txt.split(" ")[1])
+      if (response.list[i].dt_txt.split(" ")[1] === "00:00:00"){
+          console.log('WE FOUND A MATCH!!!', response.list[i])
+        fiveDayArray.push(response.list[i])
+      }
+    }
+
+
+    console.log(fiveDayArray);
+
+    // for loop thru fiveDayArray
+    // make h1s for temp ext...
+    // append them all to the html page
+    // weather: Array(1)
+    // 0:
+    // id: 804
+
+
+
+  });
+}
